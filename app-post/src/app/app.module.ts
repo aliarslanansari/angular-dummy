e068f50a05bf8e9c71d089424a8036095f9fb1fe
aliarslanansari <aliarslan1620@gmail.com>
2020-06-21 22:41:14 +0530

    added angular material

--- app-post/src/app/app.module.ts
+++ app-post/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 import { PostCreateComponent }  from './post-create/post-create.component';
 import { AppComponent } from './app.component';
@@ -17,7 +19,9 @@ import { AppComponent } from './app.component';
     BrowserModule,
     FormsModule,
     BrowserAnimationsModule,
-    MatInputModule
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
