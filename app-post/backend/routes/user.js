d58756720f943160155b500199988802aff96164
aliarslanansari <aliarslan1620@gmail.com>
2020-06-25 16:28:13 +0530

    add jwt token

--- app-post/backend/routes/user.js
+++ app-post/backend/routes/user.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const User = require('../models/user');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const user = require('../models/user');
 
 const router = express.Router();
 
@@ -41,10 +43,17 @@ router.post('/login', (req, res) => {
                 message:"Auth failed"
             });
         }
+        const token = jwt.sign(
+            {email:user.email,userId:user._id},
+            "secret_this_should_be_longer",
+            {expiresIn:'1h'});
+        res.status(200).json({
+            token:token
+        })
     })
     .catch(err=>{
         return res.status(401).json({
-            message:"Auth failed"
+            message:"Auth failed" 
         });
     })
 });
--- app-post/src/app/auth/login/login.component.ts
+++ app-post/src/app/auth/login/login.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { AuthService } from '../auth.service';
 @Component({
     templateUrl:'./login.component.html',
     styleUrls:['./login.component.css']
 })
 export class LoginComponent{
+    constructor(public authService:AuthService){}
+
     isLoading=false;
 
-    onLogin(loginForm:NgForm){
-        console.log(loginForm);
+    onLogin(form:NgForm){
+        if(form.invalid){
+            return;
+        }
+        this.authService.login(form.value.email,form.value.password);    
     }
 }
