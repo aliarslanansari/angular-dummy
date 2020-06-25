591bda3ad9c6458295bf84d9dca1112e3d145142
aliarslanansari <aliarslan1620@gmail.com>
2020-06-25 16:11:47 +0530

    add jwt token

--- app-post/backend/routes/user.js
+++ app-post/backend/routes/user.js
@@ -25,4 +25,27 @@ router.post('/signup',(req,res, next)=>{
     });
 });
 
+router.post('/login', (req, res) => {
+    User.findOne({email:req.body.email})
+    .then(user=>{
+        if(!user){
+            return res.status(401).json({
+                message:"Auth failed"
+            });
+        }
+        return bcrypt.compare(req.body.password, user.password)
+    })
+    .then(result => {
+        if(!result){
+            return res.status(401).json({
+                message:"Auth failed"
+            });
+        }
+    })
+    .catch(err=>{
+        return res.status(401).json({
+            message:"Auth failed"
+        });
+    })
+});
 module.exports = router;    
