ff27211646b845f4554391f8f7073488fe9e3221
aliarslanansari <aliarslan1620@gmail.com>
2020-06-27 15:56:31 +0530

    updated post model

--- app-post/backend/models/post.js
+++ app-post/backend/models/post.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const postSchema = mongoose.Schema({
     title: {type: String, required: true},
     content: {type: String, required:true, trim: true},
-    imagePath : {type:String,required:true}
+    imagePath : {type:String,required:true},
+    creator: {type:mongoose.Schema.Types.ObjectId, required: true, ref:'User'}
 });
 
 module.exports = mongoose.model('Post',postSchema);
