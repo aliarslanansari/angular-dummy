41718c1a788b54cc0a15cde4ad4323ab6f9b7604
aliarslanansari <aliarslan1620@gmail.com>
2020-06-28 00:32:37 +0530

    added error handling

--- app-post/src/app/error-interceptor.ts
+++ app-post/src/app/error-interceptor.ts
@@ -2,6 +2,8 @@ import { HttpInterceptor, HttpRequest, HttpHandler, HttpErrorResponse } from '@a
 import { catchError } from 'rxjs/operators';
 import { throwError } from 'rxjs';
 import { MatDialog } from '@angular/material/dialog';
+import { Injectable } from '@angular/core';
+@Injectable()
 export class ErrorInterceptor implements HttpInterceptor{
     constructor(private dialog:MatDialog){}
     intercept(req:HttpRequest<any>,next:HttpHandler){
