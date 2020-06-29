2e9b5a75a3b3018d762b7de950604c9861e97407
aliarslanansari <aliarslan1620@gmail.com>
2020-06-29 23:15:49 +0530

    error handling

--- app-post/src/app/posts/post-list/post-list.component.ts
+++ app-post/src/app/posts/post-list/post-list.component.ts
@@ -51,6 +51,8 @@ export class PostListComponent implements OnInit, OnDestroy {
   onDelete(postId:string){
     this.PostsService.deletePost(postId).subscribe(()=>{
       this.PostsService.getPosts(this.postPerPage,this.currentPage);
+    },()=>{
+      this.isLoading = false;
     });
   }
   onChangedPage(pageData : PageEvent){
