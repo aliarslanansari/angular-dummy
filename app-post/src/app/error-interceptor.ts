969abbae4db1ad1b1ddf315d1b3c2b9636d5fd55
aliarslanansari <aliarslan1620@gmail.com>
2020-06-29 22:23:50 +0530

    error handling

--- app-post/src/app/error-interceptor.ts
+++ app-post/src/app/error-interceptor.ts
@@ -10,8 +10,12 @@ export class ErrorInterceptor implements HttpInterceptor{
     intercept(req:HttpRequest<any>,next:HttpHandler){
         return next.handle(req).pipe(
             catchError((error:HttpErrorResponse)=>{
+                let errorMessage = "An unknown error occurred!"
+                if(error.error.message){
+                    errorMessage = error.error.message;
+                }
                 console.log(console.error); 
-                this.dialog.open(ErrorComponent);
+                this.dialog.open(ErrorComponent,{data:{message:errorMessage}});
                 return throwError(error);
             })
         );
--- app-post/src/app/error/error.component.ts
+++ app-post/src/app/error/error.component.ts
@@ -1,9 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
     templateUrl:'./error.component.html'
 })
 export class ErrorComponent{
-    message = 'An unknown error'
-
+    constructor(@Inject(MAT_DIALOG_DATA) public data:{message:string}){}
 }
