aa514dcaed8ad7a9fb2611e7c24c5a13668738c8
aliarslanansari <aliarslan1620@gmail.com>
2020-06-30 13:11:49 +0530

    optimization

---
+++ app-post/backend/controllers/posts.js
@@ -0,0 +1,94 @@
+exports.createPost = (req, res,next) => {
+    url = req.protocol+'://'+req.get('host');
+    const post = new Post ({
+        title:req.body.title,
+        content:req.body.content,
+        imagePath: url + "/images/" + req.file.filename,
+        creator: req.userData.userId
+    })
+    console.log(post)
+    post.save().then((results)=>{
+        res.status(201).json({
+            message: 'Post Added Successfully',
+            post:{
+                ...results,
+                id:results._id
+            }
+        });
+    })
+    .catch(error=>{
+        res.status(500).json({
+            message:'Creating a post failed!'
+        });
+    });
+}
+
+exports.updatePost = (req, res, next) => {
+    let imagePath = req.body.imagePath;
+    if (req.file) {
+      const url = req.protocol + "://" + req.get("host");
+      imagePath = url + "/images/" + req.file.filename
+    }
+    const post = {
+      title: req.body.title,
+      content: req.body.content,
+      imagePath: imagePath,
+      creator: req.userData.userIds
+    };
+    Post.updateOne({ _id: req.params.id,creator:req.userData.userId }, post).then(result => {
+        if(result.nModified>0){
+          res.status(200).json({ message: "Update successful!" });
+        }else{
+          res.status(401).json({ message: "Not Authorised" });
+        }
+    })
+    .catch(error=>{
+      res.status(500).json({
+          message:"Couldn't update the post!"
+      });
+  });
+}
+
+exports.getPosts = (req,res,next)=>{
+    const pageSize = +req.query.pagesize;
+    const currentPage = +req.query.page;
+    const postQuery = Post.find();
+    let fetchedPosts;
+    if(pageSize && currentPage){
+        postQuery.skip((currentPage-1)*pageSize)
+        .limit(pageSize);
+    }
+    postQuery.then((documents)=>{
+        fetchedPosts = documents;
+        return Post.countDocuments();
+    }).then(count=>{
+        res.status(200).json({
+            message:'Succesfully fetched',
+            posts:fetchedPosts,
+            maxPosts:count
+        });
+    })
+    .catch(error=>{
+        res.status(500).json({
+            message:"Couldn't fetch the post!"
+        });
+    });
+}
+
+exports.getPost = (req, res,next) => {
+    Post.findById(req.params.id)
+    .then(post=>{
+        if(post){
+            res.status(200).json(post);
+        }else{
+            res.status(400).json({
+              message:'Post not found'  
+            });
+        }
+    })
+    .catch(error=>{
+        res.status(500).json({
+            message:"Couldn't fetch the post!"
+        });
+    });
+}
--- app-post/backend/routes/user.js
+++ app-post/backend/routes/user.js
@@ -1,64 +1,8 @@
+const userController = require("../controllers/user")
 const express = require('express');
-const User = require('../models/user');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const user = require('../models/user');
-
 const router = express.Router();
 
-router.post('/signup',(req,res, next)=>{
-    bcrypt.hash(req.body.password, 10)
-    .then(hash=>{
-        const user = new User({
-            email: req.body.email,
-            password: hash
-        })
-        user.save().then((result)=>{
-            res.status(201).json({
-                message:'User Created',
-                result:result
-            })
-        })
-        .catch((err)=>{
-            res.status(500).json({
-                message:"Invalid Authentication Credentials!",
-            })
-        });
-    });
-});
+router.post('/signup',userController.createUser);
+router.post('/login', userController.userLogin);
 
-router.post('/login', (req, res) => {
-    let fetchedUser;
-    User.findOne({email:req.body.email})
-    .then(user=>{
-        if(!user){
-            return res.status(401).json({
-                message:"Invalid Authentication Credentials!"
-            });
-        }
-        fetchedUser = user;
-        return bcrypt.compare(req.body.password, user.password)
-    })
-    .then(result => {
-        if(!result){
-            return res.status(401).json({
-                message:"Auth failed"
-            });
-        }
-        const token = jwt.sign(
-            {email:fetchedUser.email,userId:fetchedUser._id},
-            "secret_this_should_be_longer",
-            {expiresIn:'1h'});
-        res.status(200).json({
-            token:token,
-            expiresIn:3600,
-            userId: fetchedUser._id
-        })
-    })
-    .catch(err=>{
-        return res.status(401).json({
-            message:"Auth failed" 
-        });
-    })
-});
-module.exports = router;    
+module.exports = router;
