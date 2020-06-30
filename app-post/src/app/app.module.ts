5736f77c1217638eb3e72487999615f135f48b93
aliarslanansari <aliarslan1620@gmail.com>
2020-06-30 14:33:30 +0530

    optimization

--- app-post/src/app/app.module.ts
+++ app-post/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { AngularMaterialModule } from './angular-material.module';
 import { ErrorComponent } from './error/error.component';
 import { ErrorInterceptor } from './error-interceptor';
 import { AuthInterceptor } from './auth/auth-interceptor';
@@ -8,14 +9,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule,FormsModule } from '@angular/forms';
-import { MatInputModule } from '@angular/material/input';
-import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button';
-import { MatToolbarModule } from '@angular/material/toolbar'
-import { MatExpansionModule } from '@angular/material/expansion'
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner'
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatDialogModule } from '@angular/material/dialog'
  
 import { PostCreateComponent }  from './posts/post-create/post-create.component';
 import { AppComponent } from './app.component';
@@ -40,15 +33,8 @@ import { SignUpComponent } from './auth/signup/signup.component';
     ReactiveFormsModule,
     FormsModule,
     BrowserAnimationsModule,
-    MatInputModule,
-    MatCardModule,
-    MatButtonModule,
-    MatToolbarModule,
-    MatExpansionModule,
-    MatPaginatorModule,
-    MatDialogModule,
     HttpClientModule,
-    MatProgressSpinnerModule
+    AngularMaterialModule
   ],
   providers: [
     {provide:HTTP_INTERCEPTORS,useClass:AuthInterceptor,multi:true},
